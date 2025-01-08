4e22467d2d3d4ee68c9ecfb9721808079e6b4625
Abdelrahman Yousef <abdelrahman.yousef@hadaf-hq.com>
2025-01-08 10:46:34 +0300

    update login model

--- resources/views/front/layouts/master.blade.php
+++ resources/views/front/layouts/master.blade.php
@@ -91,7 +91,58 @@
 </nav>
 
 
-<x-login-modal />
+<!-- <x-login-modal /> -->
+
+
+<!-- Modal Login -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header bg-primary text-white">
+        <h5 class="modal-title" id="loginModalLabel">Login</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <!-- Logo Section -->
+        <div class="text-center mb-4">
+          <img src="{{asset('assets/logowaffle.png')}}" alt="Logo" class="logo">
+        </div>
+
+        <form action="{{ url('login') }}" method="POST">
+            @csrf
+            <div class="mb-4">
+                <label for="email" class="form-label text-muted">Email</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                    <input type="email" class="form-control" id="email" name="email" required placeholder="Enter your email">
+                </div>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="form-label text-muted">Password</label>
+                <div class="input-group">
+                    <span class="input-group-text"><i class="fas fa-lock"></i></span>
+                    <input type="password" class="form-control" id="password" name="password" required placeholder="Enter your password">
+                </div>
+            </div>
+            <div class="mb-4 d-flex justify-content-between">
+                <div>
+                    <input type="checkbox" id="rememberMe" name="remember">
+                    <label for="rememberMe" class="form-check-label text-muted">Remember me</label>
+                </div>
+                <a href="#" class="text-primary" style="text-decoration: none;">Forgot password?</a>
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary w-100 py-2">Login</button>
+            </div>
+            <div class="mt-3 text-center">
+                <span>Don't have an account? <a href="#" data-bs-toggle="modal" data-bs-target="#signupModal" class="text-primary">Sign up</a></span>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <!-- Modal Sign Up -->
 <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
